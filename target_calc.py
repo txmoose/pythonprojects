f1a443869251304804dd0a95495e29e3cd23e749
Kyle Brieden <kyle.brieden@rackspace.com>
2014-02-02 03:22:25 -0600

    Target_cal works, but output is funky..."

---
+++ target_calc.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+Author:     Kyle Brieden
+Date:        1 Feb 14
+Updated:     1 Feb 14
+Purpose:    practice problem - Target Calculator
+
+Prompt:     Write a function that accepts two arguments: A list of integers
+            and a target value.  This function needs to perform calculations
+            to verify whether or not two of the integers in the list are able
+            to be added together to equal the target value.
+"""
+
+
+def get_valid_input():
+    values = {}
+    values['list'] = []
+    while True:
+        input_list = raw_input('Comma separated list of values: ').split(',')
+
+        if all(x.strip().isdigit() for x in input_list):
+            for x in input_list:
+                values['list'].append(int(x))
+            break
+        else:
+            print '\nPlease enter a list in this format: \"1, 2, 3, 4\"\n'
+
+    while True:
+        input_target = raw_input('Target integer: ').strip()
+
+        if input_target.isdigit():
+            values['target'] = int(input_target)
+            break
+        else:
+            print '\nPlease enter an integer to target.'
+
+    return values
+
+
+def check_list(lst, tgt):
+    answers = []
+    for x in range(len(lst)):
+        for y in lst[x:]:
+            if lst[x] + y == tgt:
+                answers.append('{} + {} = {}'.format(lst[x], y, tgt))
+    if len(lst) == 0:
+        return answers.append(
+                'No values in the list can be added to attain {}'.format(tgt))
+    else:
+        return answers
+
+
+def main():
+    values = get_valid_input()
+
+    answers = check_list(values['list'], values['target'])
+    for x in answers:
+        print x
+
+if __name__ == '__main__':
+    main()
