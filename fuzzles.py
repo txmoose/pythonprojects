1342457567c65985661be85c29661710dd2f6d0e
Kyle Brieden <kyle.brieden@rackspace.com>
2014-04-16 16:36:15 -0500

    Noo\! MR. FUZZLES\!

---
+++ fuzzles.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+print 'Will you club Mr. Fuzzles now?'
+answer = raw_input().lower()
+
+if "no" in answer or answer == "n":
+    print "Thank you!"
+else:
+    print "You monster :("
+
